98b073da82ee8376ef4faa8cf0856e2c4ed5fbc2
Christopher Hashim <christopherhashim@MacBook-Air-3.attlocal.net>
2023-05-26 21:54:38 -0700

    finished expose

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,10 @@
 // sum.test.js
 
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
+  const sum = require('../code-to-unit-test/sum.js');
+  expect(sum(1,2)).toBe(3);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1+2).toBe(3);
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,7 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('true email 1', () => {
+    let isEmailOutput = functions.isEmail("chashim@ucsd.edu");
+    expect(isEmailOutput).toBe(true);
+});
